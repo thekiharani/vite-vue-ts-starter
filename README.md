bdd5a89de8692b5306ea983f0c6135972e819339
Joe Gitonga <thekiharani@gmail.com>
2021-09-08 20:06:29 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# vite-react-ts-starter
+Vue App Starter with Vite, TypeScript and TailwindCSS
+
+Author: [Joe Gitonga(thekiharani)](https://thekiharani.com).
+
+# Quick Start
+- Install depedencies: `yarn`
+- Run Dev Server: `yarn dev`
+> Once done with development
+- Build for Production: `yarn build`
+- Preview the Final Build: `yarn serve`
+
+> More docs on the road...
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <div class="App">
+      <div class="h-screen w-full">
+        <div class="flex h-full justify-center items-center">
+          <button class="bg-blue-500 text-white font-bold px-4 py-2 rounded-lg">Submit</button>
+        </div>
+      </div>
+    </div>
+</template>
